59ea4070b38927d3da9bd1870a9f101fd1702492
tsmboa0 <tsmboa@gmail.com>
2025-10-08 12:51:50 +0100

    updated cluster1 (spl_init and spl_mint) files

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import wallet from "../../turbine-wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -8,11 +8,19 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
+const mintAuthority = keypair.publicKey;
+const decimals = 6;
 
 (async () => {
     try {
         // Start here
-        // const mint = ???
+        console.log(`Initializing mint with ${decimals} decimals`);
+        const mint = await createMint(connection, keypair, mintAuthority, null, decimals);
+
+        console.log(`Mint ${mint.toBase58()} initialized with ${decimals} decimals`);
+
+        // Result: Mint H8wnTZp7EoFER3jYhVNLBHm99xwkAkF4TuSLnUPMeC8j initialized with 6 decimals
+
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -1,6 +1,6 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
 import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import wallet from "../../turbine-wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -9,20 +9,24 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
-const token_decimals = 1_000_000n;
-
+const tokenAmount = 1_000_000n;
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("H8wnTZp7EoFER3jYhVNLBHm99xwkAkF4TuSLnUPMeC8j");
 
 (async () => {
     try {
         // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+        console.log(`Creating ATA for ${keypair.publicKey.toBase58()}`);
+        const ata = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+        // Result : Your ata is: 69icoHguopwhnwLBkTxKEUvP3mJuj7KgDXwGNirQcR1q
 
         // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
+        const mintTx = await mintTo(connection, keypair, mint, ata.address, keypair.publicKey, tokenAmount);
+        console.log(`Minted ${tokenAmount} tokens to ${ata.address.toBase58()}`);
+        // Minted 1000000 tokens to 69icoHguopwhnwLBkTxKEUvP3mJuj7KgDXwGNirQcR1q
+        console.log(`Your mint txid: ${mintTx}`);
+        // Your mint txid: 4b6Yro4VwSKqg6uayndidFGD4hKrSvUQaFkDRSz5D7xCLdJiULLhJWQU32MRzihgb2DqfsussCp5QCwJ9ZXFeZYL
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
