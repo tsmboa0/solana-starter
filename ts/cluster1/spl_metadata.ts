10ffb90075384f4c7dfab6820491ff2828484006
tsmboa0 <tsmboa@gmail.com>
2025-10-09 13:33:32 +0100

    completed splt_metadata and spl_transfer instructions

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,4 +1,4 @@
-import wallet from "../turbin3-wallet.json"
+import wallet from "../../turbine-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
 import { 
     createMetadataAccountV3, 
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("H8wnTZp7EoFER3jYhVNLBHm99xwkAkF4TuSLnUPMeC8j")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,28 +21,38 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer,
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "Casino & Church token",
+            symbol: "CNC",
+            uri: "https://www.google.com",
+            sellerFeeBasisPoints: 100,
+            creators: null,
+            collection: null,
+            uses: null
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: true,
+            collectionDetails: null
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
+        let result = await tx.sendAndConfirm(umi);
+        console.log(bs58.encode(result.signature));
+        // Result: 33KYcqrhWHFX5o1HXVTQ1H8wX7JD5Doa6ckCpCZhFE2pCZQof62Nfv4foYwhZirKbr5rvkqWRJHeuZCaxYWGPRT1
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,5 +1,5 @@
 import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../turbin3-wallet.json"
+import wallet from "../../turbine-wallet.json"
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -10,18 +10,25 @@ const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("H8wnTZp7EoFER3jYhVNLBHm99xwkAkF4TuSLnUPMeC8j");
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("HgNrCxgsuNdepRRbDsqQEYvVAnKRQS9HW7bbBMHcce8G");
 
 (async () => {
     try {
         // Get the token account of the fromWallet address, and if it does not exist, create it
+        const fromATA= await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
+        console.log(`From ATA: ${fromATA.address.toBase58()}`);
 
         // Get the token account of the toWallet address, and if it does not exist, create it
-
+        const toATA = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, to);
+        console.log(`To ATA: ${toATA.address.toBase58()}`);
         // Transfer the new token to the "toTokenAccount" we just created
+        const tx = await transfer(connection, keypair, fromATA.address, toATA.address, keypair.publicKey, 1_00_000n);
+        console.log(`Transfer success! Check out your TX here:\n\nhttps://explorer.solana.com/tx/${tx}?cluster=devnet`);
+
+        // Result: https://explorer.solana.com/tx/2VLwRrrY6ZDo5nuiauaLrjaCaJNquQD4FqSoD5Rm2Vrk4h53DdVxNJ2uAC91YaPA2BwzE1T8eGnqf4GTffNvAZKa?cluster=devnet
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
